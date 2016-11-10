428df5b8251c9db3a1a680c333d7ca1d738f9be7
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-11-10 01:50:46 -0800

    added guesser, word gen

--- public/main.js
+++ public/main.js
@@ -1,6 +1,22 @@
 var socket = io();
 var drawing;
-var drawer= true;
+var drawer;
+
+var Words = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
 
 var pictionary = function() {
 
@@ -18,23 +34,29 @@ var pictionary = function() {
 
     var guessBox;
 
-    var displayGuess= function(guess){
-          $('#showGuess h4').text(guess);
-          console.log(guess);
-        
+    var displayGuess = function(guess) {
+
+        $('#showGuess h4').text(guess);
+
+        console.log(guess);
+
     };
 
     var onKeyDown = function(event) {
         if (event.keyCode != 13) { // Enter
             return;
         }
-          var guess = guessBox.val();
 
+        var guess = guessBox.val();
         guessBox.val('');
+
         socket.emit('guess', guess);
+
         displayGuess(guess);
 
     };
+
+
 
     guessBox = $('#guess input');
     guessBox.on('keydown', onKeyDown);
@@ -59,6 +81,35 @@ var pictionary = function() {
     //      console.log(drawer);
     // });
 
+
+    socket.on('drawer', function() {
+
+     drawer = true;
+     $('#showWord').show();
+
+     
+
+      var word= Math.floor((Math.random() * Words.length) );
+
+      console.log('rrandom word is '+Words[word]);
+
+      $('#showWord h4' ).text( Words[word]);
+
+        console.log('user is a drawer');
+
+
+    });
+
+    socket.on('notDrawer', function() {
+
+     drawer = false;
+        $('.guesserUi').show();
+
+
+
+
+    });
+
     canvas.on('mousedown', function(event) {
         drawing = true;
     });
@@ -68,8 +119,9 @@ var pictionary = function() {
     });
 
     canvas.on('mousemove', function(event) {
+        console.log(drawer);
 
-          if(drawer===true){
+        if (drawer === true) {
 
             var offset = canvas.offset();
             var position = {
@@ -77,20 +129,20 @@ var pictionary = function() {
                 y: event.pageY - offset.top
             };
 
-
             if (drawing === true) {
+
                 draw(position);
                 socket.emit('draw', position);
             }
-
-          }
-
-
-
+        }
 
     });
 
     socket.on('draw', draw);
+    socket.on('reload', function(){
+      location.reload();
+    });
+
 
 };
 
@@ -98,6 +150,7 @@ var pictionary = function() {
 
 
 $(document).ready(function() {
+
 
     pictionary();
 
--- server.js
+++ server.js
@@ -7,9 +7,47 @@ app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var id;
+var drawer;
+var clients = {};
+var drawerId;
+
+var Words = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+];
+
 
 io.on('connection', function(socket) {
-  
+
+    clients[socket.id] = socket.id;
+
+
+    console.log(Object.keys(clients));
+
+
+    if (Object.keys(clients).length === 1) {
+
+        socket.emit('drawer', drawer);
+
+        drawerId= socket.id;
+
+    } else {
+        socket.emit('notDrawer', drawer);
+      console.log('not a drawer');
+    }
+
 
     socket.on('draw', function(position) {
 
@@ -17,11 +55,29 @@ io.on('connection', function(socket) {
 
     });
 
+
+
     socket.on('guess', function(guess) {
 
         socket.broadcast.emit('guess', guess);
 
         console.log(guess);
+    });
+
+    socket.on('disconnect', function() {
+
+        if(socket.id==drawerId){
+
+          console.log('drawer disconnected!!!! ');
+
+            socket.broadcast.emit('reload');
+
+        } else{
+          console.log('somebody else did');
+        }
+
+        delete clients[socket.id];
+
     });
 
 });
